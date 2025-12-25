88948cca67fa3fab71513ec917b60633ee635f83
Andrew Manga <manseband@gmail.com>
2025-12-25 10:12:11 -0500

    Add files via upload

--- play.js
+++ play.js
@@ -8,12 +8,13 @@ const params = new URLSearchParams(window.location.search);
 const levelCode = params.get("level"); // This will return String
 
 // Pre-made level codes:
-// 0d0c2-ey7pkmnohyprry0ov5ok6qr5lpjby8-1k0s101o-181a2b2d-19
-// 0e0c2-2s2l5zhrys75hkyb3p9xf6gm2uihyexo8-37363e3f-3a3c4045-3p
+// 0d0c2-ey7pkmnohyprry0ov5ok6qr5lpjby8-1k0s101o-181a2b2d-19  <- Heart
+// 0e0c2-2s2l5zhrys75hkyb3p9xf6gm2uihyexo8-37363e3f-3a3c4045-3p <- Gormless freak
 // 07082-9a91bzh5f3y-0o0u120w-0f0j171b-0v
-// 07082-9a91bzh5f3y-0o0u120w-0f0j171b-0a
-// 07072-5h8ovztj3z-0v0w0h0u-080m1014-0p
-// 07092-1w6f0msx7s7zw-0n1118-0q0x14-0u
+// 07082-9a91bzh5f3y-0o0u120w-0f0j171b-0a <- Same as above more different player start
+// 07072-5h8ovztj3z-0v0w0h0u-080m1014-0p <- Penicular
+// 07092-1w6f0msx7s7zw-0n1118-0q0w0x-0u
+// 08082-xc9ckssmqiik-0z0y0j-0k0t10-0r
 
 if (!levelCode) {
     alert("No level code provided!");
